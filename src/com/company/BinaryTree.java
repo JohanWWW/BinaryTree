1548f4cea7dd6b144d70a9e635e35f9f9b37c161
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2020-11-11 17:32:02 +0100

    commit alternate solution + fix documentation

--- src/com/company/BinaryTree.java
+++ src/com/company/BinaryTree.java
@@ -4,16 +4,6 @@ import javax.management.openmbean.KeyAlreadyExistsException;
 
 public class BinaryTree {
 
-    private static class Node {
-        Node left;
-        Node right;
-        int value;
-
-        Node(int value) {
-            this.value = value;
-        }
-    }
-
     private Node root;
 
     public void insert(int value) {
@@ -24,50 +14,99 @@ public class BinaryTree {
         /*
                 10
               /   \
-             2      16
-           /   \
-          1     4
-               / \
-              3   7
-                 / \
-                5   8
-                     \
-                      9
-                     / \
-                    null null
+             1      16
+              \
+               2
+                \
+                 4 <--
+                / \
+               3   7 <--
+                  / \
+                 5   8
+                      \
+                       9
          */
 
-        removeRecursive(root, value);
+        removeRecursive(root, null, value);
     }
 
-    private void removeRecursive(Node currentTree, int valueToRemove) {
+    private void removeRecursive(Node currentTree, Node parentTree, int valueToRemove) {
 
-        if (valueToRemove < currentTree.value) {
+        if (valueToRemove < currentTree.value && currentTree.hasLeftChild()) {
+            removeRecursive(currentTree.left, currentTree, valueToRemove);
 
-            if (currentTree.left != null) {
-                removeRecursive(currentTree.left, valueToRemove);
-            }
-
-        } else if (valueToRemove > currentTree.value) {
-
-            if (currentTree.right != null) {
-                removeRecursive(currentTree.right, valueToRemove);
-            }
+        } else if (valueToRemove > currentTree.value && currentTree.hasRightChild()) {
+            removeRecursive(currentTree.right, currentTree, valueToRemove);
 
         } else {
-            Node previous = null;
-            Node temp = currentTree;
-            while (temp.right != null) {
-                previous = temp;
-                temp = temp.right;
+            // If current is root
+            if (parentTree == null) {
+                Node left = currentTree.left;
+                root = currentTree.right;
+                insertRecursive(root, left);
+                return;
             }
 
-            if (temp.left == null) {
-                temp.value = currentTree.value;
-                previous.right = null;
-            } else {
-                temp.left.value = currentTree.value;
-                previous.left = null;
+            /*
+                IF (parent has a right child that has two children):
+
+                    ...
+                      \
+                     Node           <- "parentTree"
+                    /    \
+                  ...   Node        <- "currentTree"
+                       /    \
+                     Node   Node
+                    /    \ /    \
+                  ...   .....   ...
+
+             */
+            if (parentTree.hasRightChild() && parentTree.right.hasTwoChildren()) {
+                Node currentLeft = parentTree.right.left;
+                Node currentRight = parentTree.right.right;
+
+                parentTree.right = currentRight;
+                insertRecursive(currentRight, currentLeft);
+            }
+
+            /*
+                IF (parent has a right child which also has a right child):
+
+                    ...
+                      \
+                     Node           <- "parentTree"
+                    /    \
+                  ...   Node        <- "currentTree"
+                       /    \
+                      x    Node
+                          /    \
+                         ...   ...
+
+             */
+            else if (parentTree.hasRightChild() && parentTree.right.hasRightChild()) {
+                Node currentRight = parentTree.right.right;
+
+                parentTree.right = currentRight;
+            }
+
+            /*
+                IF (parent has a right child that have a left child):
+
+                    ...
+                      \
+                     Node       <- "parentTree"
+                    /    \
+                  ...   Node    <- "currentTree"
+                       /    \
+                     Node    x
+                    /    \
+                   ...   ...
+
+             */
+            else if (parentTree.hasRightChild() && parentTree.right.hasLeftChild()) {
+                Node currentLeft = parentTree.right.left;
+
+
             }
         }
     }
@@ -87,6 +126,22 @@ public class BinaryTree {
         }
 
         return tree;
+    }
+
+    private Node insertRecursive(Node parentNode, Node nodeToInsert) {
+        if (parentNode == null) {
+            return nodeToInsert;
+        }
+
+        if (nodeToInsert.value < parentNode.value) {
+            parentNode.left = insertRecursive(parentNode.left, nodeToInsert);
+        } else if (nodeToInsert.value > parentNode.value) {
+            parentNode.right = insertRecursive(parentNode.right, nodeToInsert);
+        } else {
+            throw new KeyAlreadyExistsException(String.format("The value '%s' already exists", parentNode.value));
+        }
+
+        return parentNode;
     }
 
     public void printTreeInOrder() {
@@ -113,5 +168,36 @@ public class BinaryTree {
         printTreeInReverseOrder(tree.right);
         System.out.println(tree.value);
         printTreeInReverseOrder(tree.left);
+    }
+
+    private static class Node {
+        Node left;
+        Node right;
+        int value;
+
+        Node(int value) {
+            this.value = value;
+        }
+
+        boolean hasLeftChild() {
+            return left != null;
+        }
+
+        boolean hasRightChild() {
+            return right != null;
+        }
+
+        boolean hasTwoChildren() {
+            return hasLeftChild() && hasRightChild();
+        }
+
+        boolean hasOneChild() {
+            return hasLeftChild() ^ hasRightChild();
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Node(%s)", value);
+        }
     }
 }
